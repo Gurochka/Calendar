ae2d7109b1333908cd4f5fd833154be1133730d0
gurochka <natali.ananyeva@gmail.com>
2019-06-20 14:41:31 +0400

    Added main parts of the app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <appHeader />
+    <mainContent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appHeader from '@/components/appHeader.vue'
+import mainContent from '@/components/mainContent.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    appHeader,
+    mainContent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/mainContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <Calendar />
+  </main>
+</template>
+
+<script>
+import Calendar from '@/components/calendar/Calendar.vue'
+
+export default {
+  components: {
+    Calendar
+  }
+}
+</script>
